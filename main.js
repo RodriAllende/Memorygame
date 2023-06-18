97dc4561afa7e88d6e513f7bce6e4482b73e827f
RodriAllende <rodriallende86@gmail.com>
2023-06-18 19:43:54 -0300

    Js Practies

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-//Init variables//
-
-let showcard=0;
-let card1=null;
-let card2=null;
-
-
-
-let numbers=[1,1,2,3,3,4,4,5,5,6,6,7,7,8,8,];
-numbers=numbers.sort(()=>{return Math.random()-0.5});
+let numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+numbers = numbers.sort(() => { return Math.random() - 0.5 });
 console.log(numbers);
 
-// Function principal
+let showcard = 0;
+let card1= null;
+let card2= null;
+let firsResults=null;
+let secondResults=null;
 
-function Show(id){
-showcard++;
+  // Variable para contar las cartas mostradas
+
+// Función principal
+function Show(id) {    
+    showcard++;
     console.log(showcard);
-}
 
-if (showcard == 1) {
-    card1= document.getElementById(id);
-    card1.innerHTML= numbers [id];
-}
+    if (showcard === 1){
+        let card1 = document.getElementById(id);
+        firsResults=numbers[id]
+        card1.innerHTML = numbers[id];
+      
+   
+
+card1.disabled = true;}}
